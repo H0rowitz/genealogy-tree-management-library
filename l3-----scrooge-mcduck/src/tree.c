da86073f3ff367fcb29702cb082cac5d9b54acc8
Pilna <pilna@outlook.fr>
2020-10-27 13:53:53 +0100

    fix unadressable access from method _delete_from_node

--- l3-----scrooge-mcduck/src/tree.c
+++ l3-----scrooge-mcduck/src/tree.c
@@ -57,8 +57,7 @@ void _show(Tree tree, Duck duck) {
 
 void _delete_from_node(Tree tree, Duck duck) {
     generic_type _duck = getMemberByName(tree, duck); 
-    delete_tree_from_node(_duck); 
-    if (strcmp(duck->name, tree->root->current->name)==0) free(tree); 
+    delete_tree_from_node(_duck);  
 }
 
 void _globalSearch(Tree tree, Duck duck, 
